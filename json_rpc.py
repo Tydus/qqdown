69b228e9631208a7928380f8ba397a621dc63254
Tydus <Tydus@Tydus.org>
2012-01-05 23:52:28 +0800

    Implemented multipart/form-data posting and some fixes

--- json_rpc.py
+++ json_rpc.py
@@ -4,6 +4,30 @@
 
 from common import *
 from urllib2 import Request
+from mimetypes import guess_type
+
+def multipart_encode(data,files):
+    BOUNDARY = '----------ThIs_Is_tHe_bouNdaRY_$'
+    CRLF = '\r\n'
+    L = []
+    if data:
+        for (key, value) in data.items():
+            L.append('--' + BOUNDARY)
+            L.append('Content-Disposition: form-data; name="%s"' % key)
+            L.append('')
+            L.append(value)
+    if files:
+        for (key, filename, value) in files:
+            L.append('--' + BOUNDARY)
+            L.append('Content-Disposition: form-data; name="%s"; filename="%s"' % (key, filename))
+            L.append('Content-Type: %s' % guess_type(filename)[0] or 'application/octet-stream')
+            L.append('')
+            L.append(value)
+    L.append('--' + BOUNDARY + '--')
+    L.append('')
+    body = CRLF.join(L)
+    content_type = 'multipart/form-data; boundary=%s' % BOUNDARY
+    return content_type, body
 
 class Json_RPC(object):
     def __init__(self):
@@ -44,7 +68,8 @@ class Json_RPC(object):
         method       'GET' or 'POST'
         query        query string passing by a dict
         data         post data passing by a dict
-        multipart    if we need multipart data, default urlencoded
+        file         post files passing by a list of 3-tuple: key, filename, data
+                     ( this indicates multipart/form-data )
         
         '''
         kwe=Entity(kwargs)
@@ -55,33 +80,38 @@ class Json_RPC(object):
         if kwe.query:
             url+="?"+urlencode(kwe.query)
 
-
-
         if method=='GET':
             request=Request(url)
         else:
-            if not kwe.data:
+            if not (kwe.data or kwe.file):
                 raise RPCError("POST with no data")
 
-            if kwe.multipart:
-                # TODO: Handle multipart here
-                raise NotImplemented
-                data=None
+            if kwe.file:
+                content_type,data=multipart_encode(kwe.data,kwe.file)
+                request=Request(url,data)
+                request.add_header('Content-Type', content_type)
             else:
                 data=urlencode(kwe.data)
-            request=Request(url,data)
+                request=Request(url,data)
 
         request.add_header('User-Agent',
             "Mozilla/5.0 (Ubuntu; X11; Linux x86_64; rv:8.0) Gecko/20100101 Firefox/8.0"
             )
+        request.add_header('Accept-Charset',"UTF-8")
+        '''
         request.add_header('Referer',
             "http://ui.ptlogin2.qq.com/cgi-bin/login?uin=&appid=567008010&f_url=loginerroralert&hide_title_bar=1&style=1&s_url=http%3A//lixian.qq.com/main.html&lang=0&enable_qlogin=1&css=http%3A//imgcache.qq.com/ptcss/r1/txyjy/567008010/login_mode_new.css%3F"
             )
+        '''
 
         response=self.opener.open(request)
+        ret=response.read()
+        response.close()
 
         #print "\033[33m"+str(self.cookie_jar)+"\033[0m"
 
-        ret=response.read()
-        response.close()
+        # FIXME: An Ugly hack to Tencent server's charset indicator using BOM header
+        if ret.startswith('\xef\xbb\xbf'):
+            ret=ret[3:]
+
         return ret
